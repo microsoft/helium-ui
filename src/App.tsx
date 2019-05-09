4bdaff720dfb65cf0d85358d34401378e21bccf2
Melanie Rush <merush@microsoft.com>
2019-05-09 13:35:36 -0600

    installed axios, grab movie/genre/actors endpoint, displays all movie types with map functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,87 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from '@material-ui/core';
 import './App.css';
+
+const heliumApi = 'https://heliumint.azurewebsites.net/api/';
+const cors = 'https://cors-anywhere.herokuapp.com/';
 
 class App extends React.Component {
 
   state = {
+    genres: [{ 
+      id: null, 
+      name: null
+    }],
+    movies: [{
+      movieId: null,
+      type: null, 
+      title: null, 
+      year: null, 
+      runtime: null,
+      genres: [], 
+      roles: [],
+    }],
+    actors: [{
+      id: null, 
+      name: null
+    }],
+  }
+
+  componentDidMount() {
+
+    // grab genre data from api
+    axios.get(cors + heliumApi + 'genres').then(response => {
+      const genreData = response.data.map((item: any) => ({
+        id: item.id,
+        name: item.genre
+      }))
+      this.setState({
+        genres: genreData
+      })
+    })
+
+    // grab movie data from api
+    axios.get(cors + heliumApi + 'movies').then(response => {
+      const moviesData = response.data.map((item: any) => ({
+        movieId: item.movieId,
+        type: item.type,
+        title: item.title,
+        year: item.year,
+        runtime: item.runtime,
+        genres: item.genres,
+        roles: item.roles
+      }))
+      this.setState({
+        movies: moviesData
+      })
+    })
+
+    // grab actor data from api
+    axios.get(cors + heliumApi + 'actors').then(response => {
+      const actorsData = response.data.map((item: any) => ({
+        id: item.id,
+        name: item.name
+      }))
+      this.setState({
+        actors: actorsData
+      })
+    })
+
+    .catch(error => {
+      console.log(error);
+    });
   }
 
   render() {
     return (
       <div>
-        <h3>Welcome</h3>
+        <h3>Movies</h3>
+        <li>
+          {this.state.movies.map(item =>(
+            <li>{item.title}</li>
+          ))}
+        </li>
       </div>
     );
   }
