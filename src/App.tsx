9b3bc1a78f287ccf18932a83bcf9259cf513ecc4
Erin Corson <frodopwns@gmail.com>
2019-05-14 09:40:24 -0600

    initial types for entities

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,53 @@ import {
   Card,
   CardContent,
   CardMedia,
-  ExpansionPanel, 
-  ExpansionPanelSummary, 
-  ExpansionPanelDetails, 
   Grid,
+  InputBase,
   Toolbar,
   Typography,
 } from '@material-ui/core';
 import CameraIcon  from '@material-ui/icons/PhotoCamera';
+import SearchIcon  from '@material-ui/icons/Search';
 import './App.css';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
+
 const heliumApi = 'https://heliumint.azurewebsites.net/api/';
 const cors = 'https://cors-anywhere.herokuapp.com/';
+
+type Movie = {
+  movieId: string,
+  type: string, 
+  title: string, 
+  year: string, 
+  runtime: number,
+  genres: string[], 
+}
+
+type Genre = {
+  id: string,
+  name: string,
+}
+
+type Actor = {
+  id: string,
+  name: string,
+}
+
 
 const divStyle = {
   color: 'blue',
   backgroundColor: 'blue',
 };
+
+interface IProps {
+  superhero: string;
+}
+
+interface IState {
+  movies: Movie[];
+  genres: Genre[];
+  actors: Actor[];
+}
 
 const cardStyle = {
   height: '100%',
@@ -30,24 +60,15 @@ const cardStyle = {
 }
 
 class App extends React.Component {
-  state = {
-    genres: [{ 
-      id: null, 
-      name: null
-    }],
-    movies: [{
-      movieId: null,
-      type: null, 
-      title: null, 
-      year: null, 
-      runtime: null,
-      genres: [], 
-      roles: [],
-    }],
-    actors: [{
-      id: null, 
-      name: null
-    }],
+  state: IState;
+
+  constructor(props: IProps) {
+    super(props);
+    this.state = { 
+      movies: [],
+      genres: [],
+      actors: [],
+    };
   }
 
   joinStr(list: string[]): string {
@@ -103,9 +124,10 @@ class App extends React.Component {
   }
 
   render() {
+
     return (
       <React.Fragment>
-      <AppBar position="static">
+      <AppBar position="sticky">
         <Toolbar>
           <CameraIcon />
           <Typography variant="h6" color="inherit" noWrap>
@@ -115,8 +137,8 @@ class App extends React.Component {
       </AppBar>
       <main>
       <Grid container spacing={8}>           
-          {this.state.movies.map(item => (
-            <Grid item sm={6} md={4} lg={3}>
+          {this.state.movies.map((item, i) => (
+            <Grid item key={i} sm={6} md={4} lg={3}>
             <Card>
               <CardContent>
                   <Typography gutterBottom variant="h5" component="h2">
@@ -147,4 +169,5 @@ class App extends React.Component {
   }
 }
 
+//export default App;
 export default App;
