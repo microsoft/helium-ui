6eafc7be06a6963269b201d745476773740db79c
Melanie Rush <merush@microsoft.com>
2019-05-21 11:34:38 -0600

    adding formik dialog! and snackbars

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from "axios";
 import Balloon from "./imgs/balloon.svg";
 import MoviePH from "./imgs/movieplaceholder.jpg";
@@ -6,6 +6,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon  from '@material-ui/icons/Search';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
+import CloseIcon from '@material-ui/icons/Close';
 import { 
   AppBar,
   Card,
@@ -23,21 +24,22 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
-  DialogContentText,
   DialogActions,
   Button,
-  TextField,
   Fab,
   CardActions,
 } from '@material-ui/core';
 import './App.css';
-import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { ifStatement } from '@babel/types';
+import { Formik, Field, Form, FormikProps } from 'formik';
+import { TextField } from 'formik-material-ui';
+import Snackbar from '@material-ui/core/Snackbar';
+
 
 const heliumApi = 'https://heliumint.azurewebsites.net/api/';
 const cors = 'https://cors-anywhere.herokuapp.com/';
 
 type Movie = {
+  id: string,
   movieId: string,
   type: string, 
   title: string,
@@ -66,6 +68,8 @@ interface IState {
   formsDialog: any,
   checkBoxDisplay: boolean,
   checkBox: boolean,
+  postSuccessAlert: boolean,
+  postFailureAlert: boolean,
 }
 
 class App extends React.Component {
@@ -77,11 +81,13 @@ class App extends React.Component {
     formsDialog: false,
     checkBoxDisplay: false,
     checkBox: false,
+    postSuccessAlert: false,
+    postFailureAlert: false,
   };
 
   joinStr(list: string[]): string {
     if (list) {
-     //  return list.join(', ')
+    //  return list.join(', ')
     }
     return ''
   }
@@ -127,33 +133,6 @@ class App extends React.Component {
     this.setState({ formsDialog: false });
   }
 
-  formsChange = (event: any) => {
-    console.log(event.target.name  + " changed")
-  }
-
-  // submit forms 
-  formsSubmit(event: any) {
-    event.preventDefault();
-
-    const newMovie = {
-      genres: [],
-      id: "12345",
-      movieId: "54312",
-      roles: [],
-      runtime: 120,
-      textSearch: "test movie",
-      title: "test movie",
-      type: "Movie",
-      year: 1994,
-    }
-
-    alert("new movie added!")
-    // submits post request of new sample movie to axios
-    axios.post(cors + heliumApi + 'movies', newMovie)
-    .then(response => console.log(response.data))
-    .catch(error => {console.log(error.response)})
-  }
-
   deleteMovie = () => {
     console.log("delete movie")
 
@@ -193,105 +172,25 @@ class App extends React.Component {
 
   render() { 
     const { anchorEl } = this.state;
-
     return (
       <React.Fragment>
-      <AppBar position="sticky">
-        <Toolbar>
-          <img src={Balloon} width="50" height="50" />
-          <Typography variant="h6" color="inherit" noWrap>
-            Helium UI
-          </Typography>          
-          <div className="searchBar">
-              <SearchIcon />
-              <InputBase
-                placeholder="Search…"
-              />
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Dialog
-            open={this.state.formsDialog}
-            onClose={this.formsClose}
-            aria-labelledby="form-dialog-title"
-          >
-            <DialogTitle id="form-dialog-title">Add New Movie</DialogTitle>
-            <DialogContent>
-              <DialogContentText>Add new movie to list of movies</DialogContentText> 
-              <TextField 
-                autoFocus
-                margin="dense"
-                name="genres"
-                label="Genres"
-                fullWidth
-                onChange={this.formsChange}
-              />
-              <TextField 
-                name="id"
-                label="ID"
-                fullWidth
-                onChange={this.formsChange}
-              />
-              <TextField 
-                name="movieId"
-                label="Movie ID"
-                fullWidth
-                onChange={this.formsChange}
-              />
-              <TextField 
-                name="roles"
-                label="Roles"
-                fullWidth
-                onChange={this.formsChange}
-              />
-              <TextField 
-                name="runtime"
-                label="RunTime"
-                fullWidth
-                onChange={this.formsChange}
-              />
-              <TextField
-                name="textSearch"
-                fullWidth 
-                onChange={this.formsChange}
-                label="Text Search"
-              />  
-              <TextField 
-                name="title"
-                fullWidth
-                label="Title"
-                placeholder="Test Movie"
-                onChange={this.formsChange}
-              />  
-              <TextField 
-                name="type"
-                fullWidth
-                label="Type"
-                value="movie"
-                onChange={this.formsChange}
-              />  
-              <TextField 
-                name="year"
-                fullWidth
-                label="Year"
-                onChange={this.formsChange}
-              />              
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={this.formsClose} color="primary">Cancel</Button>
-              <Button onClick={this.formsSubmit} color="primary">Submit</Button>
-            </DialogActions>
-          </Dialog>
-      <main>
-      <div className="fab"> 
-        <Fab className="addFAB" aria-label="addMovie" onClick={this.formsOpen} color="primary" >
-          <AddIcon />
-        </Fab>
-
-        <Fab aria-label="deleteMultipleMovie" color="secondary" onClick={this.checkBoxToggle} className="deleteFAB">
-          <DeleteIcon/>
-        </Fab>
+      <div className="appbar">
+        <AppBar position="sticky">
+          <Toolbar>
+            <img src={Balloon} width="50" height="50" />
+            <Typography variant="h6" color="inherit" noWrap>
+              Helium UI
+            </Typography>          
+            <div className="searchBar">
+                <SearchIcon />
+                <InputBase
+                  placeholder="Search…"
+                />
+            </div>
+          </Toolbar>
+        </AppBar>          
       </div>
+      <main> 
       <Grid container spacing={8}>           
           {this.state.movies.map((item, i) => (
             <Grid item key={i} sm={6} md={4} lg={3}>
@@ -310,9 +209,7 @@ class App extends React.Component {
                 />
                 <CardMedia
                   style={{height: 0, paddingTop: '56.25%'}}
-                  // image="https://storage.googleapis.com/bit-docs/bit-logo%402x.png"
                   image={MoviePH}
-
                   title="img"
                 />
                 <CardContent>
@@ -339,6 +236,143 @@ class App extends React.Component {
         <MenuItem onClick={this.deleteMovie}>Delete</MenuItem>
       </Menu>      
       </main>
+      <div className="dialogs">
+          <Dialog     
+            open={this.state.formsDialog}
+            onClose={this.formsClose}
+            aria-labelledby="form-dialog-title"
+            >
+            <DialogTitle id="form-dialog-title">Add Movie</DialogTitle>
+            <DialogContent>
+              <Formik
+                initialValues={{ id: '', year: '', runtime: 0, type: 'Movie', title: '', textSearch: '', roles: [], movieId: '', genres: [], }}
+                onSubmit={(values:Movie , actions) => {
+                  console.log(values);
+                  this.setState({ postSuccessAlert: true });
+                  
+                  // todo: add failure notification
+                  // if movie params are not full then give this postfailure message
+                  // this.setState({ postFailureAlert: true });
+
+                  
+                  // submits post request of new sample movie to axios
+                  axios.post(cors + heliumApi + 'movies', values)
+                  .then(response => console.log(response.data))
+                  .catch(error => {console.log(error.response)})
+                  }
+                }
+                render={(formikBag: FormikProps<Movie>) => (
+                  <Form autoComplete="on">
+                    <Field
+                      name="title"
+                      label="Title"
+                      type="text"
+                      component={TextField}
+                      fullWidth
+                      margin="normal"
+                    />
+                    <Field 
+                      name="year"
+                      label="Year"
+                      type="text"
+                      component={TextField}
+                      fullWidth  
+                      margin="normal"
+                    />
+                    <Field 
+                      name="runtime"
+                      label="Runtime"
+                      type="text"
+                      component={TextField}
+                      fullWidth
+                      margin="normal"            
+                    />
+                    <Field
+                      name="textSearch"
+                      label="Text Search"
+                      type="text"
+                      component={TextField}
+                      fullWidth
+                      margin="normal"
+                    />
+                    <Field
+                      name="roles"
+                      label="Roles"
+                      type="text"
+                      component={TextField}
+                      fullWidth
+                      margin="normal"
+                    />           
+                    <Field
+                      name="genres"
+                      label="Genres"
+                      type="text"
+                      component={TextField}
+                      fullWidth
+                      margin="normal"
+                    />   
+                    <Field
+                      name="movieId"
+                      label="Movie ID"
+                      type="text"
+                      component={TextField}
+                      fullWidth
+                      margin="normal"
+                    />           
+                    <Field 
+                      name="id"
+                      label="Id"
+                      type="text"
+                      component={TextField}
+                      fullWidth  
+                      margin="normal"
+                    />
+                    <Field 
+                      name="type"
+                      label="Type"
+                      type="text"
+                      value="Movie"
+                      component={TextField}
+                      fullWidth
+                      margin="normal"   
+                      InputProps={{readOnly: true}}         
+                    />
+                    <Button color="primary" onClick={this.formsClose}>Cancel</Button>
+                    <Button color="primary" type="submit" >Submit</Button>
+                  </Form>
+                )}
+              />
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        open={this.state.postSuccessAlert}
+        message={<span id="postSuccessMessage">Movie Successfully Added</span>}
+        action={[<IconButton onClick={() => this.setState({postSuccessAlert: false, formsDialog: false })}><CloseIcon color="primary" /></IconButton>]}
+      />
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        open={this.state.postFailureAlert}
+        message={<span id="postSuccessMessage">Failed to Add Movie</span>}
+        action={[<IconButton onClick={() => this.setState({postFailureAlert: false, formsDialog: false })}><CloseIcon color="primary" /></IconButton>]}
+      />
+      </div>
+      <div className="fab"> 
+        <Fab className="addFAB" aria-label="addMovie" onClick={this.formsOpen} color="primary" >
+          <AddIcon />
+        </Fab>
+        <Fab aria-label="deleteMultipleMovie" color="secondary" onClick={this.checkBoxToggle} className="deleteFAB">
+          <DeleteIcon/>
+        </Fab>
+      </div>
       <footer>
         <Typography variant="h6" align="center" gutterBottom>
         </Typography>
